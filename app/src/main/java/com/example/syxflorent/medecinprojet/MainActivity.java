73853493ed9ed3ea64ced513508ca4a9ca1f8e1b
Remy-Sonnier <remy.sonnier38@gmail.com>
2017-12-06 17:14:03 +0100

    creation de l'activity Patient (liste ) reliage de l'activity main a patient en cliquant sur le textview voir patient

--- app/src/main/java/com/example/syxflorent/medecinprojet/MainActivity.java
+++ app/src/main/java/com/example/syxflorent/medecinprojet/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.syxflorent.medecinprojet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView TextView_VoirPatients = (TextView) findViewById(R.id.TextView_VoirPatients);
+
+            TextView_VoirPatients.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intentPatient = new Intent(MainActivity.this, PatientsActivity.class);
+                startActivity(intentPatient);
+            }
+        });
     }
 }
