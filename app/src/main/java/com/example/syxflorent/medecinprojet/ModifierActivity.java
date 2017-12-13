407c6096fda7cb6c11176dc510bbdb62399add67
Remy-Sonnier <remy.sonnier38@gmail.com>
2017-12-13 15:07:24 +0100

    Ajout modifier + amelioration de cree

---
+++ app/src/main/java/com/example/syxflorent/medecinprojet/ModifierActivity.java
@@ -0,0 +1,13 @@
+package com.example.syxflorent.medecinprojet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModifierActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modifier);
+    }
+}
